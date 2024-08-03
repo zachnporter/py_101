287228a9bc2a8d2fb78f6773162276c0144f6731
Zach Porter <znporter24@gmail.com>
2024-08-03 12:25:34 -0700

    modifications per code review

--- projects/loan_calculator.py
+++ projects/loan_calculator.py
@@ -17,43 +17,24 @@ def get_number(key):
     prompt(key)
     number = input()
 
-    while invalid_number(number):
+    while not valid_number(number, key):
         prompt(f'invalid_{key}')
         number = input()
 
     return float(number)
 
-def invalid_number(num_str):
+def valid_number(num_str, key):
     try:
         float(num_str)
     except ValueError:
-        return True
+        return False
 
-    return False
+    if key == 'apr':
+        return float(num_str) >= 0
 
-def exit_calculator():
-    prompt('new_calculation')
-    answer = input()
+    return float(num_str) > 0
 
-    while not answer or answer[0].lower() not in ['y', 'n']:
-        prompt('invalid_answer')
-        answer = input()
-
-    return answer[0].lower() == 'n'
-
-os.system('clear')
-
-prompt('welcome')
-
-while True:
-    loan_amount = get_number('loan_amount')
-
-    apr = get_number('apr') / 100
-
-    loan_years = get_number('loan_years')
-
-    loan_months = loan_years * 12
-
+def calculate_monthly_payment(loan_amount, loan_months, apr):
     if apr:
         monthly_interest = apr / 12
 
@@ -64,13 +45,48 @@ while True:
     else:
         monthly_payment = loan_amount / loan_months
 
+    return monthly_payment
+
+def display_monthly_payment(monthly_payment, loan_amount, loan_years, apr):
     print('---------------------------------------')
+    print(f'{messages('amount')} {loan_amount:,.2f}')
+    print(f'{messages('years')} {loan_years:.0f}')
+    print(f'{messages('interest')} {apr * 100}%')
     print(f'{messages('monthly_payment')}{round(monthly_payment, 2)}')
     print('---------------------------------------')
 
-    if exit_calculator():
-        break
 
-    os.system('clear')
+def new_calculation():
+    prompt('new_calculation')
+    answer = input()
+
+    while not answer or answer[0].lower() not in ['y', 'n']:
+        prompt('invalid_answer')
+        answer = input()
+
+    return answer[0].lower() == 'y'
+
+os.system('clear')
+
+prompt('welcome')
+
+def run_program():
+    loan_amount = get_number('loan_amount')
+
+    apr = get_number('apr') / 100
+
+    loan_years = get_number('loan_years')
+
+    loan_months = loan_years * 12
+
+    monthly_payment = calculate_monthly_payment(loan_amount, loan_months, apr)
+
+    display_monthly_payment(monthly_payment, loan_amount, loan_years, apr)
+
+    if new_calculation():
+        os.system('clear')
+        run_program()
+
+run_program()
 
 prompt('thank_you')
